316014d57d653594bde5e4b7e8305c59acce1e29
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2024-04-19 16:35:25 +0100

    Update content.js

--- seeds/content.js
+++ seeds/content.js
@@ -1,3 +1,20 @@
+/**
+ * Set env variables for app.
+ */
+
+// Define the directory containing the .env file
+const envDir = path.resolve(__dirname, '..');
+
+// Specify the paths to the .env files
+const envFilePath = path.join(envDir, '.env');
+
+dotenv.config({ path: envFilePath });
+
+const envEnvironmentFilePath = path.join(envDir, `.env.${process.env.NODE_ENV || 'development'}`);
+
+// Load environment variables from the .env files
+dotenv.config({ path: envEnvironmentFilePath });
+
 const types = require('../constants/types');
 
 /**
