ce2022fec26a09e8591954198a86a19e810f9e9d
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2024-10-21 15:33:08 +0100

    Finished changing component structure

--- react-app/src/Visuals/Containers/index.js
+++ react-app/src/Visuals/Containers/index.js
@@ -2,3 +2,4 @@ export { default as ContentContainer } from './ContentContainer';
 export { default as LeftSiteOptionsContainer } from './LeftSiteOptionsContainer';
 export { default as RightSiteOptionsContainer } from './RightSiteOptionsContainer';
 export { default as NavContainer } from './NavContainer';
+export { default as ChatElementContainer } from './ChatElementContainer';
