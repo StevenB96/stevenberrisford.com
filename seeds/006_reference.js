ca1042a175021269b1aae70fe354006edec8f060
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2024-11-18 18:43:04 +0000

    Update 006_reference.js

--- seeds/006_reference.js
+++ seeds/006_reference.js
@@ -23,9 +23,9 @@ exports.seed = async function (knex) {
             author: 'Andrew Ward',
             role: 'CEO',
             organisation: 'Scorchsoft Ltd',
-            text: 'Steven was employed at Scorchsoft from September 28th 2020, until his resignation, in [late] August 2023. ' +
-              '[Steven] started as junior developer and was promoted to mid-level developer in July 2022. ' +
-              'He worked on several successful web and mobile application projects, using a variety of technologies.',
+            text: 'Steven was employed at Scorchsoft from September 28th 2020, until August 2023  . . .  ' +
+              'He worked on several successful web and mobile application projects, using various front-end and back-end technologies, such as React, React Native and Laravel. ' +
+              'Several of these projects were technically complex, including complex algorithms and database structures.',
             order: 1,
             created_at: new Date(),
             updated_at: new Date()
@@ -34,10 +34,9 @@ exports.seed = async function (knex) {
             author: 'Audrius Dobrovolskis',
             role: 'Lead Developer',
             organisation: 'Scorchsoft Ltd',
-            text: 'Steven made an outstanding impression on both his colleagues and myself with his exceptional technical proficiency and innovative problem-solving skills. ' +
+            text: 'Steven has been a part of our team for nearly three years  . . .  ' +
               'He has continuously demonstrated a strong grasp of the latest development trends and best practices in full-stack development . . . ' +
-              'Apart from his technical abilities, Steven is an excellent team player with strong communication skills. ' +
-              'He is always willing to listen, share his expertise, and patiently help less experienced team members.',
+              'Apart from his technical abilities, Steven is an excellent team player with strong communication skills.',
             order: 2,
             created_at: new Date(),
             updated_at: new Date()
@@ -47,9 +46,7 @@ exports.seed = async function (knex) {
             role: 'Project Team',
             organisation: 'Scorchsoft Ltd',
             text: 'I worked with Steven Berrisford at Scorchsoft Ltd., and we collaborated for over a year across several projects . . . ' +
-              'In many instances he helped challenge the team, to improve upon existing requirements without changing the core functionality, ' +
-              'and does not hesitate to delve into the details and suggest creative solutions to complex problems. ' +
-              'I would have no hesitation recommending him for any position.',
+              'He helped challenge the team, to improve upon existing requirements without changing the core functionality, and does not hesitate to delve into the details and suggest creative solutions to complex problems.',
             order: 3,
             created_at: new Date(),
             updated_at: new Date()
@@ -58,34 +55,9 @@ exports.seed = async function (knex) {
             author: 'Jordan Smith',
             role: 'Mid-Level Developer',
             organisation: 'Scorchsoft Ltd',
-            text: 'I have worked with Steven for the past two and a half years, and it has been a pleasure. ' +
-              'Steven is very detail oriented, logical and quality focused, looking for the best way and practice to build the software at hand. ' +
-              'Steven is also very easy to get along with and a great presence to have around, ' +
-              'making great effort to make time for and get to know people around him.',
-            order: 4,
-            created_at: new Date(),
-            updated_at: new Date()
-          },
-          {
-            author: 'Jordan Smith',
-            role: 'Mid-Level Developer',
-            organisation: 'Scorchsoft Ltd',
-            text: 'I have worked with Steven for the past two and a half years, and it has been a pleasure. ' +
-              'Steven is very detail oriented, logical and quality focused, looking for the best way and practice to build the software at hand. ' +
-              'Steven is also very easy to get along with and a great presence to have around, ' +
-              'making great effort to make time for and get to know people around him.',
-            order: 4,
-            created_at: new Date(),
-            updated_at: new Date()
-          },
-          {
-            author: 'Jordan Smith',
-            role: 'Mid-Level Developer',
-            organisation: 'Scorchsoft Ltd',
-            text: 'I have worked with Steven for the past two and a half years, and it has been a pleasure. ' +
-              'Steven is very detail oriented, logical and quality focused, looking for the best way and practice to build the software at hand. ' +
-              'Steven is also very easy to get along with and a great presence to have around, ' +
-              'making great effort to make time for and get to know people around him.',
+            text: 'I have worked with Steven for the past two and a half years . . . ' +
+              'He is very detail oriented, logical and quality focused, looking for the best way and practice to build the software at hand. ' +
+              'Steven is also very easy to get along with and a great presence to have around.',
             order: 4,
             created_at: new Date(),
             updated_at: new Date()
