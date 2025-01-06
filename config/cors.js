5e362b53fe3ceaa4157342c15b0136cca1c762fc
StevenB96 <stevenberrisford@gmail.com>
2025-01-06 15:05:29 +0000

    Updated domain, ip, and port configuration options

--- config/cors.js
+++ config/cors.js
@@ -3,21 +3,21 @@ const cors = require('cors');
 const nodeEnv = process.env.NODE_ENV;
 
 // Define allowed origins
-const allowedOrigins = [
-  process.env.SERVER_URL,
-];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
 
 // Configure CORS options
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
+    // Check if there is no origin or if allowedOrigins is empty or if the origin is in the allowedOrigins
+    if (!origin || allowedOrigins.length === 0 || allowedOrigins.includes(origin)) {
+      callback(null, true);  // Allow the origin
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS'));  // Reject the origin
     }
   }
 };
 
+// Export CORS configuration
 module.exports = cors(
   nodeEnv === 'production' ?
     corsOptions :
