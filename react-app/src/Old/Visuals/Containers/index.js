f914b496c12b166d163abda7a3419f8de59afeda
StevenB96 <stevenberrisford@gmail.com>
2025-01-25 15:55:15 +0000

    Removed old code

--- react-app/src/Old/Visuals/Containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ContentContainer } from './ContentContainer';
-export { default as LeftSiteOptionsContainer } from './LeftSiteOptionsContainer';
-export { default as RightSiteOptionsContainer } from './RightSiteOptionsContainer';
-export { default as NavContainer } from './NavContainer';
-export { default as ChatElementContainer } from './ChatElementContainer';
--- react-app/src/Old/Visuals/Layouts/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BaseContentElement } from './BaseContentElement';
-export { default as BaseOptionsButton } from './BaseOptionsButton';
-export { default as SiteHeader } from './SiteHeader';
-export { default as ContentGroup } from './ContentGroup';
